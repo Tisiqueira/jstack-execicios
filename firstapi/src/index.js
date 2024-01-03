704e3d64ec03b0573b65b24b399a34da258f3537
Tiago Siqueira <tiago.m.siqueira@outlook.com>
2024-01-03 00:11:31 -0300

    usando query parms
    aula mostrando como utilizar query parms

--- firstapi/src/index.js
+++ firstapi/src/index.js
@@ -1,24 +1,28 @@
 const http = require('http');
+const url = require('url');
 
 const routes = require('./routes');
 
 
 const server = http.createServer((request, response) => {
+    const parseUrl = url.parse(request.url,true);
+    console.log(parseUrl);
 
-    console.log(`Request method: ${request.method} | Request url: ${request.url}`)
+    console.log(`Request method: ${request.method} | Endpoint url: ${parseUrl.pathname}`)
 
     const route = routes.find((routeObj) => (
-        routeObj.endpoint === request.url && routeObj.method === request.method
+        routeObj.endpoint === parseUrl.pathname && routeObj.method === request.method
     ));
 
     console.log({ 'Route' : route})
 
     if(route) {
+        request.query = parseUrl.query;
         route.handler(request,response);
 
     }else{
         response.writeHead(404, {'Content-type': 'text/html'});
-        response.end(`Request method: ${request.method} | Request url: ${request.url}`) 
+        response.end(`Request method: ${request.method} | Endpoint url: ${parseUrl.pathname}`) 
     }
 });
 
